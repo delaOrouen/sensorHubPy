0c42b4e5980c843d04e18dcc85ff8a9a63245bb2
delaOrouen <rouendelao@gmail.com>
2021-05-12 19:55:38 -0700

    some changes

--- main.py
+++ main.py
@@ -17,21 +17,51 @@ CONST_FIRST_DATA_LENGTH = 12
 
 def main():
     spi = spidev.SpiDev()
-    spi.open(CONST_SPI_BUS, CONST_SPI_DEVICE)
-    spi.max_speed_hz = 976000  # somewhat conservative value
-    sensor_hub_data = []  # empty array of arrays
-    sensor_hub_data[0] = ['H'] * CONST_FIRST_DATA_LENGTH  # empty array
-    sensor_hub_data[1] = ['-1'] * 4
+    spi.open(0, 0)
+    spi.max_speed_hz = 15600000
+#     spi.cshigh = False # default false
+    spi.lsbfirst = False # default false
+    spi.mode = 0
+    spi.threewire = False # default false
+    
+    sensor_hub_data = []  # empty list of different data types
+    sensor_hub_data.insert(0,'H' * 12)  # empty array
+    #sensor_hub_data = ['-1'] * 4
     print("sanity check:")
     print('Expecting sensor_hub_data[0] to be: [HHHHHHHHHHHH]')
-    print("sensor_hub_data[0] = :", str(sensor_hub_data[0]))
+    print("sensor_hub_data[0] = ", sensor_hub_data)
 
     print("\n beginning while loop in 1 second")
     time.sleep(1)
+    recievedMessage = []
+    sendingMessage = createSendingMessage("Hello world!")
     while True:
-        sensor_hub_data[0] = spi.read(CONST_FIRST_DATA_LENGTH)
-        print("sensor_hub_data = :", str(sensor_hub_data[0]))
-        time.sleep(.1)  # wait one second
+        #sensor_hub_data.insert(0, spi.readbytes(CONST_FIRST_DATA_LENGTH))
+#         print(listToString(spi.readbytes(CONST_FIRST_DATA_LENGTH)))
+#         spi.writebytes(['*'])
+#         spi.writebytes()
+        spi.writebytes(sendingMessage)
+#         recievedMessage = spi.readbytes(12)
+#         print("recieved message: ", recievedMessage)
+#         for message in recievedMessage:
+#             if (message == 40 or message == 20):
+#                 print("Recieved Character: ", chr(message)),
+#                 time.sleep(.3)  
+        #print("sensor_hub_data = :", str(sensor_hub_data[0]))
+#         time.sleep(.2)  # wait one second
 
+
+def listToString(myList):
+    newString = ""
+    for character in myList:
+        newString += chr(character)
+    return newString
+
+def createSendingMessage(sendingMessage):
+    messageList = []
+    for character in sendingMessage:
+       messageList.append(ord(character)) 
+    return messageList
 
 main()
+                  
