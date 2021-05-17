1e880f142b141924ce582f01f7fd7d217ebfcf2e
delaOrouen <rouendelao@gmail.com>
2021-05-16 23:12:08 -0700

    i2c config

--- main.py
+++ main.py
@@ -7,50 +7,34 @@ sensors. This program will use a defined encoding to read a series of data from
 the SPI device.
 '''
 
-import spidev
+import sys
+import smbus2 as smbus  # smbus2
 import time
 
-CONST_SPI_BUS = 0
-CONST_SPI_DEVICE = 0  # we will probably only have 1 device, the stm32
-CONST_FIRST_DATA_LENGTH = 12
+I2C_SLAVE_ADDRESS = 26 # 0x1a
+MESSAGE = "Hello World!"
+
 
 
 def main():
-    spi = spidev.SpiDev()
-    spi.open(0, 0)
-    spi.max_speed_hz = 976000
-#     spi.cshigh = False # default false
-    spi.lsbfirst = False # default false
-    spi.mode = 0
-    spi.threewire = False # default false
+    I2Cbus = smbus.SMBus(1)
+    slaveSelect = 1
+    slaveAddress = I2C_SLAVE_ADDRESS
+    bytesToSend = createSendingMessage(MESSAGE)
+    sending = False
     
-    sensor_hub_data = []  # empty list of different data types
-    sensor_hub_data.insert(0,'H' * 12)  # empty array
-    #sensor_hub_data = ['-1'] * 4
-    print("sanity check:")
-    print('Expecting sensor_hub_data[0] to be: [HHHHHHHHHHHH]')
-    print("sensor_hub_data[0] = ", sensor_hub_data)
-
-    print("\n beginning while loop in 1 second")
-    time.sleep(1)
-    recievedMessage = []
-    sendingMessage = createSendingMessage("Hello world!")
     while True:
-        #sensor_hub_data.insert(0, spi.readbytes(CONST_FIRST_DATA_LENGTH))
-#         print(listToString(spi.readbytes(CONST_FIRST_DATA_LENGTH)))
-#         spi.writebytes(['*'])
-#         spi.writebytes()
-#         spi.writebytes(sendingMessage)
-#         time.sleep(.001)
-#         print("running")
-        recievedMessage = spi.readbytes(12)
-        print("recieved message: ", listToString(recievedMessage))
-#         for message in recievedMessage:
-#               if (message == 40 or message == 20):
-#                 print("Recieved Character: ", chr(message)),
-#                 time.sleep(.3)  
-        #print("sensor_hub_data = :", str(sensor_hub_data[0]))
-#         time.sleep(.2)  # wait one second
+        if (sending):
+            I2Cbus.write_i2c_block_data(slaveAddress, 0x00, bytesToSend)
+            print("sent data:", MESSAGE)
+        else:
+            try:
+                data = I2Cbus.read_i2c_block_data(slaveAddress, 0x00, 16)
+                print("data received: ")
+                print(data)
+            except:
+                print("remote i/o error")
+                time.sleep(0.5)
     
 
 
@@ -61,10 +45,10 @@ def listToString(myList):
     return newString
 
 def createSendingMessage(sendingMessage):
-    messageList = []
+    byteList = []
     for character in sendingMessage:
-       messageList.append(bytes(character, 'ascii', 'replace'))
-    return messageList
+       byteList.append(ord(character))
+    return byteList
 
 main()
                   
