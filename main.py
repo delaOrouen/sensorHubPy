af73098b3734032b5eb0698a7086e3e01505f867
delaOrouen <rouendelao@gmail.com>
2021-05-17 15:10:07 -0700

    working uart

--- main.py
+++ main.py
@@ -1,41 +1,28 @@
 #!/usr/bin/python
 '''
 Developer: Rouen de la O
-Purpose: This is a program designed to operate a raspberry pi as a SPI master.
-This master will read from a SPI slave which is connected to a variety of
-sensors. This program will use a defined encoding to read a series of data from
-the SPI device.
+
+
+pin configuration is enabled/disabled at /boot/config.txt
 '''
 
-import sys
-import smbus2 as smbus  # smbus2
+#import sys
+#import smbus2 as smbus  # smbus2
 import time
+import serial
 
-I2C_SLAVE_ADDRESS = 26 # 0x1a
+# I2C_SLAVE_ADDRESS = 0x1a # 0x1a, 26
 MESSAGE = "Hello World!"
 
 
 
 def main():
-    I2Cbus = smbus.SMBus(1)
-    slaveSelect = 1
-    slaveAddress = I2C_SLAVE_ADDRESS
-    bytesToSend = createSendingMessage(MESSAGE)
-    sending = False
-    
+    ser = serial.Serial("/dev/ttyS0", baudrate = 9600, parity=serial.PARITY_NONE, stopbits=serial.STOPBITS_ONE, bytesize=serial.EIGHTBITS, timeout=1)
     while True:
-        if (sending):
-            I2Cbus.write_i2c_block_data(slaveAddress, 0x00, bytesToSend)
-            print("sent data:", MESSAGE)
-        else:
-            try:
-                data = I2Cbus.read_i2c_block_data(slaveAddress, 0x00, 16)
-                print("data received: ")
-                print(data)
-            except:
-                print("remote i/o error")
-                time.sleep(0.5)
-    
+        rdata = ser.read(12)
+        print(rdata)
+        time.sleep(.010)
+        
 
 
 def listToString(myList):
@@ -51,4 +38,30 @@ def createSendingMessage(sendingMessage):
     return byteList
 
 main()
+
+# i2c main
+# def main():
+#     I2Cbus = smbus.SMBus(1)
+#     time.sleep(1)
+#     slaveSelect = 1
+#     slaveAddress = I2C_SLAVE_ADDRESS
+#     bytesToSend = createSendingMessage(MESSAGE)
+#     sending = False
+#     convertedData = ""
+#     
+#     while True:
+#         if (sending):
+#             I2Cbus.write_i2c_block_data(slaveAddress, 0x00, bytesToSend)
+#             print("sent data:", MESSAGE)
+#         else:
+#             try:
+#                 data = I2Cbus.read_i2c_block_data(slaveAddress, 0x00, 12)
+#                 if (data[0] != 211):
+#                     print("data received: ", data)
+#                     convertedData = listToString(data)
+#                     print("               ", convertedData)
+#                     time.sleep(0.1)
+#             except:
+#                 print("remote i/o error")
+#                 time.sleep(0.5)
                   
