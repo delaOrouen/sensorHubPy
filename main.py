0ea1553342c8d908c2ada8d12a47ac5494e9d0c0
Christian Lancaster <christianlanx@gmail.com>
2021-05-18 14:24:56 -0700

    put it to the server thing

--- main.py
+++ main.py
@@ -5,65 +5,46 @@ Developers: Rouen de la O, Christrian Lancaster
 
 File summary
 '''
-
-import time
-import serial
-from gpiozero import LED
-
 # sensorhub
 import SensorHub
 
 # other stuff
-# import logging
-# from flask import Flask, send_file, request, Response
-# from prometheus_client import start_http_server, Gauge, generate_latest
+import logging
+from flask import Flask, send_file, request, Response
+from prometheus_client import start_http_server, Gauge, generate_latest
 
-# I2C_SLAVE_ADDRESS = 0x1a # 0x1a, 26
-MESSAGE = "Hello World!"
-
-
-def test():
-    sensorhubInfinite = SensorHub.SensorHub()
-    dataRecieved = ""
-    print("starting")
-    while True:
-        dataRecieved =  sensorhubInfinite.readBytes(12)
-        print(dataRecieved)
-#         time.sleep(.010)
+# initialize the sensorhub
+sensorhub = SensorHub.SensorHub()
 
 
-test()
-# # initialize the sensorhub
-# sensorhub = SensorHub.SensorHub()
-#
-# # PROMETHEUS FLASK SERVER
-# logger = logging.getLogger(__name__)
-#
-# app = Flask(__name__)
-#
-# CONTENT_TYPE_LATEST = str('text/plain; version=0.0.4; charset=utf-8')
-#
-# # create metrics to track sensor data
-# SENSORHUB_NOISELEVEL = Gauge(
-#     'sensorhub_noiselevel_db',
-#     'Noise level observed by the Sensor Hub'
-# )
-#
-# # configure the metrics endpoint
-# @app.route('/metrics', methods=['GET'])
-# def get_data():
-#     """Returns all data as plaintext."""
-#     try:
-#         SENSORHUB_NOISELEVEL.set(sensorhub.readBytes())
-#     except Exception as e:
-#         logger.error("Failed to update noise level. Exception: {}".format(e))
-#
-#     return Response(generate_latest(), mimetype=CONTENT_TYPE_LATEST)
-#
-#
-# # run the web server
-# if __name__ == '__main__':
-#     app.run(debug=True, host='0.0.0.0')
+# PROMETHEUS FLASK SERVER
+logger = logging.getLogger(__name__)
+
+app = Flask(__name__)
+
+CONTENT_TYPE_LATEST = str('text/plain; version=0.0.4; charset=utf-8')
+
+# create metrics to track sensor data
+SENSORHUB_NOISELEVEL = Gauge(
+    'sensorhub_noiselevel_db',
+    'Noise level observed by the Sensor Hub'
+)
+
+# configure the metrics endpoint
+@app.route('/metrics', methods=['GET'])
+def get_data():
+    """Returns all data as plaintext."""
+    try:
+        SENSORHUB_NOISELEVEL.set(sensorhub.readBytes())
+    except Exception as e:
+        logger.error("Failed to update noise level. Exception: {}".format(e))
+
+    return Response(generate_latest(), mimetype=CONTENT_TYPE_LATEST)
+
+
+# run the web server
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
 
 # BASIC FLASK SERVER
 # app = Flask(__name__)
