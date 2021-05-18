6f970b01cf3088465e0ad79d98093231df58b638
Christian <christianlanx@gmail.com>
2021-05-17 20:51:39 -0700

    added some codes

--- main.py
+++ main.py
@@ -1,56 +1,4 @@
-# sensorhub
-import sensorhub
 
-# other stuff
-import logging
-from flask import Flask, send_file, request, Response
-from prometheus_client import start_http_server, Gauge, generate_latest
-
-# initialize the sensorhub
-I2C_ADDR = 26 # 0x1a
-sensorhub = sensorhub.SensorHub(I2C_ADDR)
-
-# PROMETHEUS FLASK SERVER
-logger = logging.getLogger(__name__)
-
-app = Flask(__name__)
-
-CONTENT_TYPE_LATEST = str('text/plain; version=0.0.4; charset=utf-8')
-
-# create metrics to track sensor data
-SENSORHUB_NOISELEVEL = Gauge(
-    'sensorhub_noiselevel_db',
-    'Noise level observed by the Sensor Hub'
-)
-
-# configure the metrics endpoint
-@app.route('/metrics', methods=['GET'])
-def get_data():
-    """Returns all data as plaintext."""
-    try:
-        SENSORHUB_NOISELEVEL.set(sensorhub.recvBytes())
-    except Exception as e:
-        logger.error("Failed to update noise level. Exception: {}".format(e))
-    
-    return Response(generate_latest(), mimetype=CONTENT_TYPE_LATEST)
-
-# run the web server
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
-
-# BASIC FLASK SERVER
-# app = Flask(__name__)
-
-# @app.route('/')
-# def getSensorDataJson():
-#     sensorData = {
-#         "Noise Level": "%0.1f dB" % sensorhub.recvBytes()
-#     }
-#     return json.dumps(sensorData)
-
-# if __name__ == '__main__':
-#     app.run(host='0.0.0.0', port=9100)
-#!/usr/bin/python
 '''
 Developer: Rouen de la O
 
@@ -121,3 +69,58 @@ main()
 #             except:
 #                 print("remote i/o error")
 #                 time.sleep(0.1)
+
+
+# # sensorhub
+# import sensorhub
+
+# # other stuff
+# import logging
+# from flask import Flask, send_file, request, Response
+# from prometheus_client import start_http_server, Gauge, generate_latest
+
+# # initialize the sensorhub
+# I2C_ADDR = 26 # 0x1a
+# sensorhub = sensorhub.SensorHub(I2C_ADDR)
+
+# # PROMETHEUS FLASK SERVER
+# logger = logging.getLogger(__name__)
+
+# app = Flask(__name__)
+
+# CONTENT_TYPE_LATEST = str('text/plain; version=0.0.4; charset=utf-8')
+
+# # create metrics to track sensor data
+# SENSORHUB_NOISELEVEL = Gauge(
+#     'sensorhub_noiselevel_db',
+#     'Noise level observed by the Sensor Hub'
+# )
+
+# # configure the metrics endpoint
+# @app.route('/metrics', methods=['GET'])
+# def get_data():
+#     """Returns all data as plaintext."""
+#     try:
+#         SENSORHUB_NOISELEVEL.set(sensorhub.recvBytes())
+#     except Exception as e:
+#         logger.error("Failed to update noise level. Exception: {}".format(e))
+    
+#     return Response(generate_latest(), mimetype=CONTENT_TYPE_LATEST)
+
+# # run the web server
+# if __name__ == '__main__':
+#     app.run(debug=True, host='0.0.0.0')
+
+# # BASIC FLASK SERVER
+# # app = Flask(__name__)
+
+# # @app.route('/')
+# # def getSensorDataJson():
+# #     sensorData = {
+# #         "Noise Level": "%0.1f dB" % sensorhub.recvBytes()
+# #     }
+# #     return json.dumps(sensorData)
+
+# # if __name__ == '__main__':
+# #     app.run(host='0.0.0.0', port=9100)
+# #!/usr/bin/python
